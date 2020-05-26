6d1a307bacc8e0d1a381d82929908fa9fa351492
Bogdan Mironov <mad.rod123@outlook.com>
2020-05-27 01:59:16 +0300

    Initial commit

---
+++ API2/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+
+namespace api3.Controllers
+{
+    public class HostPointController : Controller
+    {
+        private readonly ILogger<HostPointController> _logger;
+        WebClient client = new WebClient();
+        public HostPointController(ILogger<HostPointController> logger)
+        {
+            _logger = logger;
+        }
+
+        public byte[] GetFile(string filter)
+        {
+            string address = "http://hidemyna.me/ru/api/proxylist.php?&code=688296646857634&" + filter.Replace(';', '&');
+            byte[] data = client.DownloadData(address);
+            return data;
+        }
+    }
+}
